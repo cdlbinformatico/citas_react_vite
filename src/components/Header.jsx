87fa6d5833d501fc03377283628a9d543ab2567d
cdlbinformatico <cdlb.informatico@gmail.com>
2022-03-06 17:34:15 -0300

    no mucho

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <> 
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
             Seguimiento Pacientes AAA {''}
-            <span className="text-indigo-600">Veterinaria</span>
+            <span className="text-indigo-700">Veterinaria</span>
         </h1>
     </>
   )
