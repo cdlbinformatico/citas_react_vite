7a55d34dada93ef7efe93638b133851d57fc0b6b
cdlbinformatico <cdlb.informatico@gmail.com>
2022-03-06 17:38:28 -0300

    prueba segundo commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
   return (
     <> 
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            Seguimiento Pacientes AAA {''}
-            <span className="text-indigo-700">Veterinaria</span>
+            Seguimiento Pacientes {''}
+            <span className="text-indigo-600">Veterinaria</span>
         </h1>
     </>
   )
